d1c2e6467a08ddeb7f7ca2aef2813822350e4d62
Troy Starwalt <troy.starwalt@gmail.com>
2015-12-16 10:32:39 -0500

    updated pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,11 @@ class PagessController < ApplicationController
 
 
 	def index
-		@users = Users.all.count
-		if user_signed_in?
-			redirect_to profile_path(current_user)
-		else
-			render root_path
-		end
+		# @users = Users.all.count
+		# if user_signed_in?
+		# 	redirect_to profile_path(current_user)
+		# else
+		# 	render root_path
+		# end
 	end
 end
