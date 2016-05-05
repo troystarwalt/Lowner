eb3d0b53f98f836edaa950981cda949249e82a66
Troy Starwalt <troy.starwalt@gmail.com>
2016-05-05 16:25:02 -0400

    beginning changes to root

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,6 +1,6 @@
 # Setting the HighVoltage to set the index is the site's root.
 
 HighVoltage.configure do |config|
-	config.home_page = 'index'
+	config.home_page = 'welcome'
 	config.layout = false
 end
